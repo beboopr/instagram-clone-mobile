1f1e97cba09b431e981e5a47a3cffe7b3b57af95
beboopr <linnets-router.09@icloud.com>
2022-11-10 10:51:05 -0500

    ig-clone

---
+++ photolist.js
@@ -0,0 +1,46 @@
+// put a fake list of photos intead of hitting an api
+
+const photolist = [{
+     id: 1, 
+    photoURL: "https://images.unsplash.com/photo-1666858094442-6b2a6592bdf8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTI2Nw&ixlib=rb-4.0.3&q=80&w=1080",
+    description: 'Person riding a rollerskate down the street',
+    userName: 'WeatherNut',
+    profilePhoto: 'https://randomuser.me/api/portraits/men/1.jpg',
+},
+{
+    id: 2,
+    photoURL: "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80",
+    description: 'Hand holding a light bulb',
+    userName: 'Meta Monster',
+    profilePhoto: 'https://randomuser.me/api/portraits/women/2.jpg',
+},
+{
+    id: 3,
+    photoURL:"https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+    description:'Plane in the middle of the forest',
+    userName: 'Bart',
+    profilePhoto: 'https://randomuser.me/api/portraits/men/3.jpg',
+},
+{
+    id: 4,
+    photoURL: "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    description: 'A view of a snowy the moutains',
+    userName: 'Treehugger',
+    profilePhoto: 'https://randomuser.me/api/portraits/women/4.jpg',
+},
+{
+    id: 5,
+    photoURL: "https://images.unsplash.com/photo-1502230831726-fe5549140034?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    description: 'rocks on top of eachother mantaining balance',
+    userName: 'Nooneme',
+    profilePhoto: 'https://randomuser.me/api/portraits/women/5.jpg',
+},
+{
+    id: 6,
+    photoURL:"https://images.unsplash.com/photo-1472898965229-f9b06b9c9bbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description:'Open Sing',
+    userName: 'Silly Dad',
+    profilePhoto: 'https://randomuser.me/api/portraits/men/6.jpg',
+}]
+
+export default photolist;
